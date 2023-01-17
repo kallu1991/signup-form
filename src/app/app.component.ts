d447a7aa8e02daa521cbd67b1d98f4911a28bde8
Kalyan Patibandla <kp21333@corp.ojas-it.com>
2023-01-17 11:30:09 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'signup';
+  signupForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.signupForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength]],
+      lastName: ['', [Validators.required, Validators.minLength]],
+      email: ['', [Validators.required, Validators.minLength]],
+      password: ['', [Validators.required, Validators.minLength]],
+    });
+  }
+
+  ngOnInit(): void {};
+
+  onSubmit(){
+    console.log(this.signupForm.value);
+    
+  }
 }
